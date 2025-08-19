fc72ced9a168f7ded8956dcd5f911a52998fcf03
Jash Patwa <patwajash24@gmail.com>
2025-08-19 10:05:30 +0530

    Functions

---
+++ 1st.c
@@ -0,0 +1,78 @@
+#include<stdio.h>
+void main()
+{
+    // int a = 10;
+    // float b = 10.15;
+    // char c = 'J';
+    // const float pi = 3.14;
+    // printf("This is value of a : %d",a);
+    // printf("\nThis is value of b : %f",b);
+    // printf("\nThis is value of c : %c",c);
+    // printf("\nThis is value of pi : %f",pi);
+
+
+    // char Name;
+    // int age;
+    // printf("\nEnter your Name initial : ");
+    // scanf("%c", &Name);
+    // printf("\nEnter your age : ");
+    // scanf("%d", &age);
+    // printf("\nYour Name initial is : %c and Your age is : %d", Name,age);
+
+    // printf("Your name is Jash!");
+
+    // int a = 15,b = 10;
+    // printf("\nAddition of a and b is : %d",a+b);
+
+    // int r;
+    // printf("\nEnter area of circle: ");
+    // scanf("%d",&r);
+    // float area = ((22/7)*r*r);
+    // printf("\nArea of circle is : %f",area);
+
+    // int c;
+    // printf("\nEnter celcius : ");
+    // scanf("%d",&c);
+    // float f = (c*9/5) + 32;
+    // printf("\nConversion from celcius %d to fahrenheit is : %f ", c, f);
+
+    // int age,salary;
+    // printf("Enter your age: ");
+    // scanf("%d",&age);
+    // if (age>=18){
+    //     printf("you are above 18, so you can vote.");
+    // }
+    // else{
+    //     printf("you are below 18, so you can't vote.");
+    // }
+
+    // int i,j=10,total;
+    // printf("Enter a number: ");
+    // scanf("%d",&i);
+    // while (j<=i)
+    // {
+    //     total = i*j;
+    //     printf("%d * %d = %d\n",i,j,total);
+    //     j++;
+    // }
+
+    // do{
+    //     total = i * j;
+    //     printf("%d * %d = %d\n", i, j, total);
+    //     j++;
+    // }while (j<=i);
+
+    // for (j=1;j<=i;j++)
+    // {
+    //     total = i * j;
+    //     printf("%d * %d = %d\n",i,j,total);
+    // }
+    
+    // while (j>=1)
+    // {
+    //     total = i*j;
+    //     printf("%d * %d = %d\n",i,j,total);
+    //     j--;
+    // }
+    
+}
