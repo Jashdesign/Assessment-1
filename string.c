72959f0766464fb4b7eb6cc0052e51f306c46ecf
Jash Patwa <patwajash24@gmail.com>
2025-08-22 12:33:59 +0530

    strings

---
+++ string.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<string.h>
+void main(){
+    // char Name[50];
+    // printf("Enter your name: ");
+    // scanf("%s", &Name);
+    // int j = strlen(Name);
+    // for (int i = 0; i <= j; i++)
+    // {
+    //     printf("%c\n", Name[i]);
+    // }
+
+    // char str1[] = {"Jash"};
+    // char str2[] = {"Jash"};
+    // int str3 = strcmp(str1,str2);
+    // printf("%d",str3);
+
+    // char str1[] = {"Jash"};
+    // char str2[30];
+    // printf("%s", strcpy(str2,str1));
+
+    
+}
